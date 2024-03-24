0ee917984098a8ae5397600b50d3b739040405f5
jaymind2810 <jaymind2810@gmail.com>
2024-03-24 20:15:58 +0530

    [17.0][ADD] Added Carzone Theme.

---
+++ theme_carzone/__manifest__.py
@@ -0,0 +1,58 @@
+{
+    'name': 'Carzone Theme',
+    'description': 'carzone website theme',
+    'category': 'Theme',
+    'sequence': 1001,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/header.xml',
+        'views/footer.xml',
+        'views/homepage.xml'
+    ],
+    'images': [
+        
+    ],
+    'assets':{
+        'web.assets_frontend': [
+            'theme_carzone/static/src/scss/style.scss',
+            'theme_carzone/static/src/scss/animate.min.scss',
+            'theme_carzone/static/src/scss/bootstrap.min.scss',
+            'theme_carzone/static/src/scss/bootstrap-select.min.scss',
+            'theme_carzone/static/src/scss/bootstrap-submenu.scss',
+            'theme_carzone/static/src/scss/dropzone.scss',
+            'theme_carzone/static/src/scss/jquery.mCustomScrollbar.scss',
+            'theme_carzone/static/src/scss/lightbox.min.scss',
+            'theme_carzone/static/src/scss/magnific-popup.scss',
+            'theme_carzone/static/src/scss/slick.scss',
+            'theme_carzone/static/src/scss/skins/red.scss',
+            'theme_carzone/static/src/scss/ie10-viewport-bug-workaround.scss',
+            'theme_carzone/static/src/scss/jnoty.scss',
+            # 'theme_carzone/static/src/js/app.js',
+            # 'theme_carzone/static/src/js/bootstrap.min.js',
+            # 'theme_carzone/static/src/js/bootstrap-select.min.js',
+            # 'theme_carzone/static/src/js/bootstrap-submenu.js',
+            # 'theme_carzone/static/src/js/dropzone.js',
+            # 'theme_carzone/static/src/js/ie10-viewport-bug-workaround.js',
+            # 'theme_carzone/static/src/js/ie-emulation-modes-warning.js',
+            # 'theme_carzone/static/src/js/jnoty.js',
+            # 'theme_carzone/static/src/js/jquery.countdown.js',
+            # 'theme_carzone/static/src/js/jquery.easing.1.3.js',
+            # 'theme_carzone/static/src/js/jquery.filterizr.js',
+            # 'theme_carzone/static/src/js/jquery.magnific-popup.min.js',
+            # 'theme_carzone/static/src/js/jquery.mb.YTPlayer.js',
+            # 'theme_carzone/static/src/js/jquery.mCustomScrollbar.concat.min.js',
+            # 'theme_carzone/static/src/js/jquery.mousewheel.min.js',
+            # 'theme_carzone/static/src/js/jquery.scrollUp.js',
+            # 'theme_carzone/static/src/js/jquery-2.2.0.min.js',
+            # 'theme_carzone/static/src/js/lightgallery-all.js',
+            # 'theme_carzone/static/src/js/popper.min.js',
+            # 'theme_carzone/static/src/js/rangeslider.js',
+            # 'theme_carzone/static/src/js/slick.min.js',
+        ],
+    },
+    'configurator_snippets': {
+        
+    },
+    'license': 'LGPL-3',
+}
